8894d43c5cad55a81ce132b7858158e54ff41b6a
pierrdu <pierre@duhem.com>
2019-02-26 17:29:39 +0100

    2.1.0

--- acp/multilinks_module.php
+++ acp/multilinks_module.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * @package phpBB Extension - LMDI Multilinks
-* @copyright (c) 2017 Pierre Duhem - LMDI
+* @copyright (c) 2017-2019 Pierre Duhem - LMDI
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
@@ -15,25 +15,23 @@ class multilinks_module {
 	protected $path_helper;
 	protected $ext_path;
 	protected $config_text;
+	protected $table;
 
 	public function main ($id, $mode)
 	{
-		global $user, $config, $template, $request, $phpbb_container;
+		global $user, $config, $db, $template, $request, $phpbb_container, $table_prefix;
 
 		$this->path_helper = $phpbb_container->get('lmdi.multilinks.multilinks_path_helper');
 		$this->ext_path = $this->path_helper->get_ext_path_web ();
-		// var_dump ($this->ext_path);
 
 		$form_name = 'acp_multilinks';
-		$config_text_pp = 'lmdi_multilinks_pp';
-		$config_text_ap = 'lmdi_multilinks_ap';
+		$this->table = $table_prefix . 'lmdi_multilinks';
 
-		$this->config_text = $phpbb_container->get('config_text');
 		$user->add_lang_ext ('lmdi/multilinks', 'acp_multilinks');
 		$this->tpl_name = 'acp_multilinks_body';
 		$this->page_title = $user->lang['ACP_MULTILINKS_TITLE'];
 
-		$ppap = $request->variable ('ppap', '');	// 'pp' = prepend, 'ap' = append
+		$ppap = $request->variable ('ppap', 0); // false = prepend, true = append
 
 		if (version_compare ($config['version'], '3.2.x', '<'))
 		{
@@ -62,7 +60,6 @@ class multilinks_module {
 			}
 		}
 
-		// var_dump ($action);
 		if ($action)
 		{
 			switch ($action)
@@ -72,81 +69,10 @@ class multilinks_module {
 					$token = $request->variable ('hash', '');
 					if (check_link_hash ($token, $form_name))
 					{
-						if ($ppap == 'ap')
-						{
-							$otext = $config_text_ap;
-							$dtext = $config_text_pp;
-							$orig = $this->config_text->get ($config_text_ap);
-							$dest = $this->config_text->get ($config_text_pp);
-						}
-						else
-						{
-							$otext = $config_text_pp;
-							$dtext = $config_text_ap;
-							$orig = $this->config_text->get ($config_text_pp);
-							$dest = $this->config_text->get ($config_text_ap);
-						}
-						$orows = json_decode ($orig, true);
-						$drows = json_decode ($dest, true);
-						$nbdest = count ($drows);
-						if ($nbdest >= 5)
-						{
-							trigger_error($user->lang['ACP_ML_TABLE_FULL'] . adm_back_link($this->u_action), E_USER_WARNING);
-						}
-						$uid = $request->variable('uid', -1);
-						$orow = $orows[$uid];
-						$drows[] = $orow;
-						$drows = array_values ($drows);
-						$dest = json_encode ($drows);
-						$this->config_text->set ($dtext, $dest);
-						unset ($orows[$uid]);
-						$orows = array_values ($orows);
-						$orig = json_encode ($orows);
-						$this->config_text->set ($otext, $orig);
-						trigger_error($user->lang['MULTILINK_CONFIG_UPDATED'] . adm_back_link($this->u_action));
-					}
-					else
-					{
-						trigger_error($user->lang['FORM_INVALID'] . adm_back_link($this->u_action));
-					}
-				break;
-				// Item move up
-				case 'move_up' :
-					$token = $request->variable ('hash', '');
-					if (check_link_hash ($token, $form_name))
-					{
-						$uid = $request->variable('uid', -1);
-						$links = $this->config_text->get ('lmdi_multilinks_'.$ppap);
-						$rows = json_decode ($links, true);
-						$row0 = $rows[$uid-1];
-						$row1 = $rows[$uid];
-						$rows[$uid-1] = $row1;
-						$rows[$uid] = $row0;
-						$rows = array_values ($rows);
-						$links = json_encode ($rows);
-						$this->config_text->set ('lmdi_multilinks_'.$ppap, $links);
-						trigger_error($user->lang['MULTILINK_CONFIG_UPDATED'] . adm_back_link($this->u_action));
-					}
-					else
-					{
-						trigger_error($user->lang['FORM_INVALID'] . adm_back_link($this->u_action));
-					}
-				break;
-				// Item move down
-				case 'move_down' :
-					$token = $request->variable ('hash', '');
-					if (check_link_hash ($token, $form_name))
-					{
-						$uid = $request->variable('uid', -1);
-						$links = $this->config_text->get ('lmdi_multilinks_'.$ppap);
-						$rows = json_decode ($links, true);
-						$row0 = $rows[$uid+1];
-						$row1 = $rows[$uid];
-						$rows[$uid+1] = $row1;
-						$rows[$uid] = $row0;
-						$rows = array_values ($rows);
-						$links = json_encode ($rows);
-						$this->config_text->set ('lmdi_multilinks_'.$ppap, $links);
+						$uid = (int) $request->variable('uid', -1);
+						$var_ppap = (int) !$ppap;
+						$sql = "UPDATE " . $this->table . " SET ppap = $var_ppap WHERE item_id = $uid";
+						$db->sql_query($sql);
 						trigger_error($user->lang['MULTILINK_CONFIG_UPDATED'] . adm_back_link($this->u_action));
 					}
 					else
@@ -158,13 +84,9 @@ class multilinks_module {
 				case 'delete' :
 					if (confirm_box(true))
 					{
-						$uid = $request->variable('uid', -1);
-						$links = $this->config_text->get ('lmdi_multilinks_'.$ppap);
-						$rows = json_decode ($links, true);
-						unset ($rows[$uid]);
-						$rows = array_values ($rows);
-						$links = json_encode ($rows);
-						$this->config_text->set ('lmdi_multilinks_'.$ppap, $links);
+						$uid = (int) $request->variable('uid', -1);
+						$sql = "DELETE FROM " . $this->table . " WHERE item_id = $uid";
+						$db->sql_query($sql);
 						trigger_error($user->lang['MULTILINK_CONFIG_UPDATED'] . adm_back_link($this->u_action));
 					}
 					else
@@ -182,26 +104,28 @@ class multilinks_module {
 				// Item edition
 				case 'edit' :
 					$uid = $request->variable('uid', -1);
-					$links = $this->config_text->get ('lmdi_multilinks_'.$ppap);
-					$rows = json_decode ($links, true);
-					$row = $rows[$uid];
+					$sql = "SELECT * FROM " . $this->table . " WHERE item_id = $uid";
+					$result = $db->sql_query($sql);
+					$row = $db->sql_fetchrow($result);
 					$template->assign_vars(array(
 						'S_320'		=> $mlinks_320,
 						'URL_ID'		=> $uid,
 						'ANCHOR'		=> $row['anchor'],
 						'TITLE'		=> $row['title'],
 						'URL'		=> $row['url'],
+						'ENABLED'		=> $row['enabled']==true ? 'CHECKED' : '',
 						'BLANK'		=> $row['blank']==true ? 'CHECKED' : '',
 						'MLICON'		=> $row['icon'],
-						'MLFILE'		=> $row['file'],
-						'ICON_YES'	=> $row['uticon']==true ? 'CHECKED' : '',
-						'ICON_NO'		=> $row['uticon']==false ? 'CHECKED' : '',
-						'FILE_YES'	=> $row['utfile']==true ? 'CHECKED' : '',
-						'FILE_NO'		=> $row['utfile']==false ? 'CHECKED' : '',
+						'MLFILE'		=> $row['pict'],
+						'ICON_YES'	=> $row['usicon']==true ? 'CHECKED' : '',
+						'ICON_NO'		=> $row['usicon']==false ? 'CHECKED' : '',
+						'FILE_YES'	=> $row['usfile']==true ? 'CHECKED' : '',
+						'FILE_NO'		=> $row['usfile']==false ? 'CHECKED' : '',
 						'PP_ACTION'	=> $this->u_action . '&amp;action=edit&amp;ppap='.$ppap,
 						'S_ED_URL'	=> true,
 						'S_ED_PP'	=> $ppap == 'pp' ? true : false,
 						));
+					$db->sql_freeresult($result);
 				break;
 				// New item creation
 				case 'add' :
@@ -211,6 +135,7 @@ class multilinks_module {
 						'ANCHOR'		=> '',
 						'TITLE'		=> '',
 						'URL'		=> '',
+						'ENABLED'		=> '',
 						'BLANK'		=> '',
 						'MLICON'		=> '',
 						'MLFILE'		=> '',
@@ -223,7 +148,7 @@ class multilinks_module {
 						'S_ADD_PP'	=> $ppap == 'pp' ? true : false,
 						));
 				break;
-				// New or edited idem saving
+				// Saving the new or edited item
 				case 'save' :
 					if (!check_form_key($form_name))
 					{
@@ -231,101 +156,139 @@ class multilinks_module {
 					}
 					$this->validation_data ($mlinks_320, $ppap);
 				break;
+				// Moving records up and down
+				case 'move_up':
+					$uid = $request->variable ('uid', 0);
+					$pid = $request->variable ('pid', 0);
+					$sql1 = "UPDATE " . $this->table . " SET item_id = 0 WHERE item_id = $uid";
+					$sql2 = "UPDATE " . $this->table . " SET item_id = $uid WHERE item_id = $pid";
+					$sql3 = "UPDATE " . $this->table . " SET item_id = $pid WHERE item_id = 0";
+					$db->sql_query($sql1);
+					$db->sql_query($sql2);
+					$db->sql_query($sql3);
+					trigger_error($user->lang['MULTILINK_CONFIG_UPDATED'] . adm_back_link($this->u_action));
+				break;
+				case 'move_down':
+					$uid = $request->variable ('uid', 0);
+					$nid = $request->variable ('nid', 0);
+					$sql1 = "UPDATE " . $this->table . " SET item_id = 0 WHERE item_id = $uid";
+					$sql2 = "UPDATE " . $this->table . " SET item_id = $uid WHERE item_id = $nid";
+					$sql3 = "UPDATE " . $this->table . " SET item_id = $uid WHERE item_id = 0";
+					$db->sql_query($sql);
+					$db->sql_query($sql1);
+					$db->sql_query($sql2);
+					trigger_error($user->lang['MULTILINK_CONFIG_UPDATED'] . adm_back_link($this->u_action));
+				break;
 			}
 		}
+		// Default action = display
 		else
 		{
-			$nbpp = $this->count_items ($config_text_pp);
-			$nbap = $this->count_items ($config_text_ap);
-			$this->assign_block_vars ($config_text_pp, 'mlpp', 'pp', $form_name, $nbap);
-			$this->assign_block_vars ($config_text_ap, 'mlap', 'ap', $form_name, $nbpp);
-			$s_disable_pp = false;
-			if ($nbpp >= 5)
-			{
-				$s_disable_pp = true;
-			}
-			$s_disable_ap = false;
-			if ($nbap >= 5)
-			{
-				$s_disable_ap = true;
-			}
+			$this->assign_block_vars ('mlpp', 0 /* _PP_ */, $form_name);
+			$this->assign_block_vars ('mlap', 1 /* _AP_ */, $form_name);
 			$pict = $this->ext_path . 'adm/style/icon_trans.gif';
 			$pictno = $this->ext_path . 'adm/style/icon_trans_disabled.gif';
 			$altstr = $user->lang['ACP_ML_TRANSFER'];
 			$template->assign_vars(array(
-				'PP_ACTION'		=> $this->u_action . '&amp;action=add&amp;ppap=pp',
-				'AP_ACTION'		=> $this->u_action . '&amp;action=add&amp;ppap=ap',
+				'PP_ACTION'		=> $this->u_action . '&amp;action=add&amp;ppap=0', // _PP_
+				'AP_ACTION'		=> $this->u_action . '&amp;action=add&amp;ppap=1', // _AP_
 				'S_CONFIG_PAGE'	=> true,
 				'U_ACTION'		=> $this->u_action,
 				'ICON_ML_TRANSFER'	=> "<img src=\"$pict\" alt=\"$altstr\" title=\"$altstr\" />",
 				'ICON_ML_TRANSNO'	=> "<img src=\"$pictno\" alt=\"$altstr\" title=\"$altstr\" />",
-				'S_DISABLE_PP'		=> $s_disable_pp == true ? true : false,
-				'S_DISABLE_AP'		=> $s_disable_ap == true ? true : false,
 				));
 		}
 		add_form_key ($form_name);
 	}
 
 
-	private function count_items ($text)
+	private function assign_block_vars ($block, $ppap, $form_name)
 	{
-		$links = $this->config_text->get ($text);
-		$rows = json_decode ($links, true);
-		$nb = count ($rows);
-		return $nb;
-	}
+		global $template, $db;
 
-
-	private function assign_block_vars ($text, $block, $ppap, $form_name)
-	{
-		global $template;
-
-		$links = $this->config_text->get ($text);
-		$rows = json_decode ($links, true);
-		$nb = count ($rows);
-		for ($i = 0; $i < $nb; $i++)
+		$sql = "SELECT * FROM " . $this->table . " WHERE ppap = $ppap ORDER BY enabled DESC";
+		$result = $db->sql_query($sql);
+		// Compute the set of ids
+		$idset = array();
+		$cpteur = 0;
+		while ($row = $db->sql_fetchrow($result))
 		{
-			$row = $rows[$i];
+			$uid = $row['item_id'];
+			$idset[] = $uid;
+			$cpteur++;
+		}
+		// Rewind the result
+		$db->sql_rowseek (0, $result);
+		// Compute the table lines
+		$cpteur = 0;
+		$nb = count ($idset);
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$uid = $row['item_id'];
+			$str_pid = "&amp;pid=";
+			$str_nid = "&amp;nid=";
+			if ($cpteur)
+			{
+				$pid = $idset[$cpteur - 1];
+				$str_pid .= $pid;
+			}
+			if ($cpteur < $nb)
+			{
+				$nid = $idset[$cpteur];
+				$str_nid .= $nid;
+			}
+			$str_checked = "<input type='checkbox' name='enabled' checked disabled>";
+			$str_unchecked = "<input type='checkbox' name='enabled' disabled>";
 			$template->assign_block_vars($block, array(
 				'NAME'			=> $row['anchor'],
 				'TITLE'			=> $row['title'],
 				'URL'			=> $row['url'],
-				'U_ML_MOVE_UP'		=> $this->u_action . "&amp;action=move_up&amp;ppap=$ppap&amp;uid=$i&amp;hash=" . generate_link_hash($form_name),
-				'U_ML_MOVE_DOWN'	=> $this->u_action . "&amp;action=move_down&amp;ppap=$ppap&amp;uid=$i&amp;hash=" . generate_link_hash($form_name),
-				'U_ML_EDIT'		=> $this->u_action . "&amp;action=edit&amp;ppap=$ppap&amp;uid=$i",
-				'U_ML_DELETE'		=> $this->u_action . "&amp;action=delete&amp;ppap=$ppap&amp;uid=$i",
-				'U_ML_TRANSFER'	=> $this->u_action . "&amp;action=transfer&amp;ppap=$ppap&amp;uid=$i&amp;hash=" . generate_link_hash($form_name),
+				'ENABLED'			=> $row['enabled'] ? $str_checked : $str_unchecked,
+				'U_ML_MOVE_UP'		=> $this->u_action . "&amp;action=move_up&amp;ppap=$ppap&amp;uid=$uid$str_pid&amp;hash=" . generate_link_hash($form_name),
+				'U_ML_MOVE_DOWN'	=> $this->u_action . "&amp;action=move_down&amp;ppap=$ppap&amp;uid=$uid$str_nid&amp;hash=" . generate_link_hash($form_name),
+				'U_ML_EDIT'		=> $this->u_action . "&amp;action=edit&amp;ppap=$ppap&amp;uid=$uid",
+				'U_ML_DELETE'		=> $this->u_action . "&amp;action=delete&amp;ppap=$ppap&amp;uid=$uid",
+				'U_ML_TRANSFER'	=> $this->u_action . "&amp;action=transfer&amp;ppap=$ppap&amp;uid=$uid&amp;hash=" . generate_link_hash($form_name),
 				'U_ML_TRANSNO'		=> '',
 				));
+			$cpteur ++;
 		}
+		$db->sql_freeresult($result);
+	}
+
+	private function sanity_check ($var, $length)
+	{
+		global $db;
+		$var = trim ($var);
+		$var = substr ($var, 0, $length);
+		$var = $db->sql_escape ($var);
+		return ($var);
 	}
 
 
 	private function validation_data ($mlinks_320, $ppap)
 	{
-		global $request, $user;
+		global $request, $user, $db;
 
 		$uid = $request->variable('uid', -1);
-		$links = $this->config_text->get ('lmdi_multilinks_'.$ppap);
-		$rows = json_decode ($links, true);
-		$nbdest = count ($rows);
-		if ($nbdest >= 5 && $uid == -1)
-		{
-			trigger_error($user->lang['ACP_ML_TABLE_FULL'] . adm_back_link($this->u_action), E_USER_WARNING);
-		}
-		$uid = $request->variable('uid', -1);
 		$anchor = $request->variable ('ml_anchor', '', true);
+		$anchor = $this->sanity_check ($anchor, 32);
 		$title = $request->variable ('ml_title', '', true);
-		$url = $request->variable ('ml_url', '', true);
-		$blank = $request->variable('ml_blank', false);
+		$title = $this->sanity_check ($title, 64);
 		$icon = $request->variable ('ml_icon', '', true);
-		$uticon = $request->variable('use_icon', false);
-		$file = $request->variable ('ml_file', '', true);
-		$utfile = $request->variable('use_file', false);
+		$icon = $this->sanity_check ($icon, 32);
+		$pict = $request->variable ('ml_file', '', true);
+		$pict = $this->sanity_check ($pict, 512);
+		$url = $request->variable ('ml_url', '', true);
+		$url = $this->sanity_check ($url, 512);
+		$enabled = (int) $request->variable('ml_enabled', false);
+		$blank = (int) $request->variable('ml_blank', false);
+		$usicon = (int) $request->variable('use_icon', false);
+		$usfile = (int) $request->variable('use_file', false);
 		// Using icon by default
-		if (!$utfile && !$uticon)
+		if (!$usfile && !$usicon)
 		{
-			$uticon = true;
+			$usicon = 1;
 		}
 		// Default icons if empty
 		if (empty ($icon))
@@ -347,21 +310,30 @@ class multilinks_module {
 				$icon = 'fa-' . $icon;
 			}
 		}
-		if (empty ($file))
-		{
-			$utfile = false;
-		}
 		if ($uid == -1)
 		{
-			$rows[] = array ('anchor' => $anchor, 'title' => $title, 'url' => $url, 'blank' => $blank, 'icon' => $icon, 'uticon' => $uticon, 'file' => $file, 'utfile' => $utfile);
+			$sql = "INSERT INTO ". $this->table ."
+				(anchor, title, icon, pict, url, ppap, enabled, blank, usicon, usfile) 
+				VALUES ('$anchor', '$title', '$icon', '$pict', '$url', $ppap, $enabled, $blank, $usicon, $usfile)";
+			$db->sql_query($sql);
+			$item_id = $db->sql_nextid();
 		}
 		else
 		{
-			$rows[$uid] = array ('anchor' => $anchor, 'title' => $title, 'url' => $url, 'blank' => $blank, 'icon' => $icon, 'uticon' => $uticon, 'file' => $file, 'utfile' => $utfile);
+			$sql  = "UPDATE ". $this->table ." SET
+				anchor = '$anchor',
+				title = '$title',
+				icon = '$icon',
+				pict = '$pict',
+				url = '$url',
+				ppap = '$ppap',
+				enabled = $enabled,
+				blank = $blank,
+				usicon = $usicon,
+				usfile = $usfile
+				WHERE item_id = $uid";
+			$db->sql_query($sql);
 		}
-		$rows = array_values ($rows);
-		$links = json_encode ($rows);
-		$this->config_text->set ('lmdi_multilinks_'.$ppap, $links);
 		trigger_error($user->lang['MULTILINK_CONFIG_UPDATED'] . adm_back_link($this->u_action));
 	}
 }
